d6dc9aebe44eb38928337ca51fba5545a74ff9e9
Amin Benarieb <amin.benarieb.work@gmail.com>
2017-04-01 05:39:23 +0300

    Added 64base image encoding and decoding

---
+++ final/iOSClient/HuaweiCliend/UIImage+Base64.swift
@@ -0,0 +1,24 @@
+//
+//  UIImage+Base64.swift
+//  Emojuirity
+//
+//  Created by Amin Benarieb on 4/1/17.
+//  Copyright © 2017 Amin Benarieb. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+	
+	public func base64() -> String? {
+		let imageData = UIImagePNGRepresentation(self)
+		return imageData?.base64EncodedString()
+	}
+}
+
+extension String {
+	func base64() -> UIImage? {
+		let dataDecoded = Data(base64Encoded: self, options: NSData.Base64DecodingOptions(rawValue: 0))!
+		return UIImage(data: dataDecoded)
+	}
+}
